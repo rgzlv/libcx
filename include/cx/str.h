94c6b7b238e041f45ca7672f60b34e4fd05a4ae2
rgz <rgz@rgz.lv>
2025-01-20 03:10:11 +0200

    str: new functions strtoupper, strtoupper_i, strtolower, strtolower_i, strpfx, strsfx (changed), strmatch, strhaspfx, strhassfx

--- include/cx/str.h
+++ include/cx/str.h
@@ -1,9 +1,18 @@
 #ifndef CX_STR_H
 #define CX_STR_H
 
+#include <stddef.h>
 #include <stdbool.h>
 
 char *cx_strcat(const char *s1, const char *s2);
-bool cx_strsfx(const char *s, const char *suffix);
+char *cx_strtoupper(const char *s);
+char *cx_strtoupper_i(char *s);
+char *cx_strtolower(const char *s);
+char *cx_strtolower_i(char *s);
+char *cx_strpfx(const char *s, size_t len);
+char *cx_strsfx(const char *s, size_t len);
+bool cx_strmatch(const char *s, const char *match);
+bool cx_strhaspfx(const char *s, const char *prefix);
+bool cx_strhassfx(const char *s, const char *suffix);
 
 #endif
--- src/str.c
+++ src/str.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
 #include <cx/str.h>
 
@@ -15,8 +16,79 @@ char *cx_strcat(const char *s1, const char *s2) {
 	return s;
 }
 
-bool cx_strsfx(const char *s, const char *suffix) {
-	if (!s || !suffix || !*suffix) return false;
+char *cx__strto(char *dst, char *src, int (*to_fn)(int)) {
+	if (!dst || !src || !to_fn) return NULL;
+	for (char *s = src, *d = dst; *s; d++, s++) *d = to_fn(*s);
+	return dst;
+}
+
+char *cx_strtoupper(const char *s) {
+	if (!s) return NULL;
+	size_t len = strlen(s);
+	char *dst = malloc(len + 1);
+	if (!dst) return NULL;
+	dst[len] = 0;
+	return cx__strto(dst, (char *)s, toupper);
+}
+
+char *cx_strtoupper_i(char *s) {
+	if (!s) return NULL;
+	size_t len = strlen(s);
+	return cx__strto(s, (char *)s, toupper);
+}
+
+char *cx_strtolower(const char *s) {
+	if (!s) return NULL;
+	size_t len = strlen(s);
+	char *dst = malloc(len + 1);
+	if (!dst) return NULL;
+	dst[len] = 0;
+	return cx__strto(dst, (char *)s, tolower);
+}
+
+char *cx_strtolower_i(char *s) {
+	if (!s) return NULL;
+	size_t len = strlen(s);
+	return cx__strto(s, (char *)s, tolower);
+}
+
+char *cx_strpfx(const char *s, size_t len) {
+	if (!s || !len) return NULL;
+	char *prefix = malloc(len + 1);
+	if (!prefix) return NULL;
+	prefix[len] = 0;
+	return strncpy(prefix, s, len);
+}
+
+char *cx_strsfx(const char *s, size_t len) {
+	if (!s || !len || !*s) return NULL;
+	const char *c = s;
+	while (*++c);
+	c -= len;
+	if (c < s) c = s;
+	char *suffix = malloc(len + 1);
+	if (!suffix) return NULL;
+	suffix[len] = 0;
+	return memcpy(suffix, c, len);
+}
+
+bool cx_strmatch(const char *s, const char *match) {
+	if (!s || !match) return false;
+	for (const char *c = s; *c; c++)
+		for (const char *m = match; *c && *m && *c == *m; c++, m++)
+			if (!*(m + 1)) return true;
+	return false;
+}
+
+bool cx_strhaspfx(const char *s, const char *prefix) {
+	if (!s || !prefix) return false;
+	for (const char *c = s; *c && *prefix; c++, prefix++)
+		if (*c != *prefix) return false;
+	return true;
+}
+
+bool cx_strhassfx(const char *s, const char *suffix) {
+	if (!s || !suffix) return false;
 	char *last = strrchr(s, *suffix);
 	if (!last) return false;
 	if (!strcmp(last, suffix)) return true;
