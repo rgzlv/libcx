20bc2da2eec6d4aee6a442f6f2ae299d92a496f0
rgz <rgz@rgz.lv>
2025-02-10 21:24:34 +0200

    err: handle NULL error string

--- src/err.c
+++ src/err.c
@@ -11,9 +11,11 @@ char *cx_progname;
 void cx_err(const char *fmt, ...) {
 	if (cx_progname) fprintf(stderr, "%s: ", cx_progname);
 	va_list ap;
-	va_start(ap, fmt);
-	vfprintf(stderr, fmt, ap);
-	va_end(ap);
+	if (fmt) {
+		va_start(ap, fmt);
+		vfprintf(stderr, fmt, ap);
+		va_end(ap);
+	} else fputs("error", stderr);
 	if (errno) fprintf(stderr, ": %s", strerror(errno));
 	fputc('\n', stderr);
 	exit(EXIT_FAILURE);
@@ -21,10 +23,12 @@ void cx_err(const char *fmt, ...) {
 
 void cx_errx(const char *fmt, ...) {
 	if (cx_progname) fprintf(stderr, "%s: ", cx_progname);
-	va_list ap;
-	va_start(ap, fmt);
-	vfprintf(stderr, fmt, ap);
-	va_end(ap);
+	if (fmt) {
+		va_list ap;
+		va_start(ap, fmt);
+		vfprintf(stderr, fmt, ap);
+		va_end(ap);
+	} else fputs("error", stderr);
 	fputc('\n', stderr);
 	exit(EXIT_FAILURE);
 }
